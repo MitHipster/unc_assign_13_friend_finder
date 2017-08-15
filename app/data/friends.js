787a7d0411fd11350e17023da902b5bd1f2426d3
Tim Acker <tjacker31@gmail.com>
2017-08-14 22:54:21 -0400

    Friends Array
    Created additional objects in the friends array.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,63 @@
 var friendsArr = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-      "5",
-      "1",
-      "4",
-      "4",
-      "5",
-      "1",
-      "2",
-      "5",
-      "4",
-      "1"
-    ]
+    "name": "Janet",
+    "photo": "http://www.designskilz.com/random-users/images/imageF15.jpg",
+    "scores": ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+  },
+  {
+    "name": "Taylor",
+    "photo": "http://www.designskilz.com/random-users/images/imageM49.jpg",
+    "scores": ["2", "5", "2", "3", "3", "1", "4", "4", "4", "2"]
+  },
+  {
+    "name": "Michelle",
+    "photo": "http://www.designskilz.com/random-users/images/imageF35.jpg",
+    "scores": ["2", "3", "2", "3", "1", "4", "1", "5", "1", "5"]
+  },
+  {
+    "name": "Jessica",
+    "photo": "http://www.designskilz.com/random-users/images/imageF43.jpg",
+    "scores": ["1", "3", "5", "4", "5", "4", "2", "2", "5", "2"]
+  },
+  {
+    "name": "Tyler",
+    "photo": "http://www.designskilz.com/random-users/images/imageM2.jpg",
+    "scores": ["1", "4", "1", "4", "1", "2", "5", "5", "5", "1"]
+  },
+  {
+    "name": "Vanessa",
+    "photo": "http://www.designskilz.com/random-users/images/imageF41.jpg",
+    "scores": ["4", "5", "4", "1", "2", "4", "2", "1", "3", "3"]
+  },
+  {
+    "name": "Jennifer",
+    "photo": "http://www.designskilz.com/random-users/images/imageF4.jpg",
+    "scores": ["1", "5", "3", "5", "4", "1", "4", "2", "4", "4"]
+  },
+  {
+    "name": "Paul",
+    "photo": "http://www.designskilz.com/random-users/images/imageM17.jpg",
+    "scores": ["3", "3", "3", "2", "1", "5", "1", "2", "2", "3"]
+  },
+  {
+    "name": "Fiona",
+    "photo": "http://www.designskilz.com/random-users/images/imageF7.jpg",
+    "scores": ["2", "3", "5", "2", "3", "3", "1", "2", "5", "4"]
+  },
+  {
+    "name": "Adam",
+    "photo": "http://www.designskilz.com/random-users/images/imageM38.jpg",
+    "scores": ["4", "5", "4", "3", "1", "3", "3", "2", "3", "1"]
+  },
+  {
+    "name": "Elizabeth",
+    "photo": "http://www.designskilz.com/random-users/images/imageF31.jpg",
+    "scores": ["1", "4", "5", "5", "3", "4", "3", "4", "4", "2"]
+  },
+  {
+    "name": "Robert",
+    "photo": "http://www.designskilz.com/random-users/images/imageM4.jpg",
+    "scores": ["2", "2", "1", "5", "3", "2", "3", "3", "1", "1"]
   }
 ];
 
